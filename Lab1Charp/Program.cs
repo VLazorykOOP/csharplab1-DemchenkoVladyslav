71675dc49fdbfa0f2ca7e4abee7885ee586421c3
ExOwLcIsT <demchenkovlad80@gmail.com>
2024-02-18 17:53:09 +0200

    made 5 tasks

--- Lab1Charp/Program.cs
+++ Lab1Charp/Program.cs
@@ -1,16 +1,161 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System.Text;
 
- static void task1()
+class Program
 {
-    Console.WriteLine("Task1 !");
-    Console.Write("s= ");
-    string? str = Console.ReadLine();
-    float s = 0; 
-    if (str != null) s = float.Parse(str);
-    double p = 4 * Math.Sqrt(s);
-    Console.WriteLine("p=" + p);
+    static void task1()
+    {
+        Console.WriteLine("Task1 !");
+        float s1 = 0;
+        float s2 = 0;
+        while (true)
+        {
+            try
+            {
+                Console.Write("s1 = ");
+                string str = Console.ReadLine();
+                s1 = float.Parse(str);
+                Console.Write("s2 = ");
+                str = Console.ReadLine();
+                s2 = float.Parse(str);
+                break;
+            }
+            catch (Exception e)
+            {
+                Console.Clear();
+                continue;
+            }
+        }
+        double p = (s1 * s1 * s1 + s2 * s2 * s2) / 2;
+        Console.WriteLine("p = " + p);
 
+    }
+    static void task2()
+    {
+        Console.WriteLine("Task1 !");
+        int m, n;
+        while (true)
+        {
+            try
+            {
+                Console.Write("M = ");
+                m = int.Parse(Console.ReadLine());
+                Console.Write("N = ");
+                n = int.Parse(Console.ReadLine());
+                break;
+            }
+            catch (Exception e)
+            {
+                Console.Clear();
+                continue;
+            }
+        }
+        if (m % n == 0)
+            Console.WriteLine("M/N = " + m / n);
+        else
+            Console.WriteLine("M на N націло не ділиться");
+    }
+    static void task3()
+    {
+        int x, y;
+        while (true)
+        {
+            try
+            {
+                Console.Write("x = ");
+                x = int.Parse(Console.ReadLine());
+                Console.Write("y = ");
+                y = int.Parse(Console.ReadLine());
+                break;
+            }
+            catch (Exception e)
+            {
+                Console.Clear();
+                continue;
+            }
+        }
+        if (x > 0 || x * x + y * y > 64 || x * x + y * y < 9)
+        {
+            Console.WriteLine("Ні");
+        }
+        if (x * x + y * y == 64 || x * x + y * y == 9 || (x == 0 && ((y <= 8 && y >= 3) || (y > -8 && y < -3))))
+        {
+            Console.WriteLine("На межі");
+        }
+        else
+        {
+            Console.WriteLine("Так");
+        }
+    }
+    static void task4()
+    {
+        string[] cards = {"туз",
+            "король",
+            "дама",
+            "валет",
+            "десятка",
+            "дев'ятка",
+            "вісімка",
+            "сімка",
+            "шістка"};
+        int k;
+        while (true)
+        {
+            try
+            {
+                Console.Write("k = ");
+                k = int.Parse(Console.ReadLine());
+                if (k < 6 || k > 14)
+                    throw new Exception();
+                break;
+            }
+            catch (Exception e)
+            {
+                Console.Clear();
+                continue;
+            }
+        }
+        Console.WriteLine(cards[14 - k]);
+    }
+    public static double chastka(int n1, int n2)
+    {
+        if (n2 == 0)
+            throw new DivideByZeroException();
+        return n1 / n2;
+    }
+    static void task5()
+    {
+        int x, y;
+        while (true)
+        {
+            try
+            {
+                Console.Write("x = ");
+                x = int.Parse(Console.ReadLine());
+                Console.Write("y = ");
+                y = int.Parse(Console.ReadLine());
+                break;
+            }
+            catch (Exception e)
+            {
+                Console.Clear();
+                continue;
+            }
+        }
+        try
+        {
+            Console.WriteLine(chastka(x, y));
+        }
+        catch (Exception e)
+        { Console.WriteLine(e.Message); }
+    }
+    public static void Main(String[] args)
+    {
+        Console.OutputEncoding = UTF8Encoding.UTF8;
+        Console.WriteLine("Lab 1 !");
+        //task1();
+        //task2();
+        //task3();
+        //task4();
+        //task5();
+    }
 }
-Console.WriteLine("Lab 1 !");
-task1();
-// continue ...
